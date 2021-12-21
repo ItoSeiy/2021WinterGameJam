0211d6f2d29aab604a6e023206b4b4b8737f5712
伊東聖矢 <tvedaamamesab@gmail.com>
2021-12-21 12:06:30 +0900

    GameManager作成中

---
+++ Assets/Ito/Scripts/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance => _instance;
+
+
+    static GameManager _instance;
+    public List<string> ItemId { get => _itemId;}
+
+
+
+    private List<string> _itemId = new List<string>();
+   
+
+    private void Awake()
+    {
+        _instance = this;
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    public void GameStart()
+    {
+
+    }
+
+    public void GetItem(string itemId)
+    {
+        _itemId.Add(itemId);
+
+    }
+
+
+}
