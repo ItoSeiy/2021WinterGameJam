b2defa35bba693cd7dc6364ecdad8a6403da8b4d
Ryu-0824 <83778052+Ryuki-Arai@users.noreply.github.com>
2021-12-21 18:36:23 +0900

    シーン遷移etc.

---
+++ Assets/RyukiArai/ResultTree.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResultTree : MonoBehaviour
+{
+    [SerializeField] GameObject[] items = default;
+
+    List<int> itemID = GameObject.Find("GameManager").GetComponent<GameManager>().ItemId;
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach(var go in itemID)
+        {
+            Instantiate(items[go],new Vector3(Random.Range(-10,10), Random.Range(5,10),0),transform.rotation.normalized);
+        }
+    }
+}
