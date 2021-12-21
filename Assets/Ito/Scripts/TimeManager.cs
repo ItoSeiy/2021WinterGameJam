3de97672994b03e3a2997a7e26d9378b560a65d8
伊東聖矢 <tvedaamamesab@gmail.com>
2021-12-21 14:39:11 +0900

    TimeManager作成

---
+++ Assets/Ito/Scripts/TimeManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeManager : MonoBehaviour
+{
+    public static TimeManager Instance => _instance;
+    private  static TimeManager _instance;
+
+    public float GameTimer { get => _gameTimer; }
+    public bool IsFeverTime { get => _isFeverTime; }
+
+    [SerializeField] private float _gameTimer = 60f;
+    [SerializeField] private float _feverTime = 10f;
+    private bool _isFeverTime = false;
+    private void Awake()
+    {
+        _instance = this;
+    }
+    private void Update()
+    {
+        if (!GameManager.Instance.IsStart) return;
+        _gameTimer -= Time.deltaTime;
+
+        if(_gameTimer <= _feverTime)
+        {
+            _isFeverTime = true;
+        }
+    }
+}
