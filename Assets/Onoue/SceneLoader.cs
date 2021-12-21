cb2899a55f0d9c76748e4344e4a4d634e7647558
OnoueHiroaki <hirophon18@ezweb.ne.jp>
2021-12-21 17:14:28 +0900

    シーンローダーをシングルトンにしました。

--- Assets/Onoue/SceneLoader.cs
+++ Assets/Onoue/SceneLoader.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class SceneLoader : MonoBehaviour
 {
+    public static SceneLoader Instance => _instance;
+    private static SceneLoader _instance;
+    private void Awake()
+    {
+        _instance = this;
+    }
     public void LoadScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
