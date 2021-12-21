36e7cece136aa59b26ece765ca64e091ced92ec8
OnoueHiroaki <hirophon18@ezweb.ne.jp>
2021-12-21 16:51:06 +0900

    スタークラスの修正

--- Assets/Onoue/Item/Star.cs
+++ Assets/Onoue/Item/Star.cs
@@ -4,6 +4,6 @@ using UnityEngine;
 
 public class Star : ItemBase, IItem
 {
-    public ItemID ID { get => ItemID.Star; }
-    public int Score { get => 4; }
+    public ItemID ID => ItemID.Star;
+    public int Score => 4;
 }
