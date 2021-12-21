0c1ea0421728c7100d9203f8b4bcad06c9a08a04
Ryu-0824 <83778052+Ryuki-Arai@users.noreply.github.com>
2021-12-21 13:53:58 +0900

    アイテム処理待ち

--- Assets/RyukiArai/PlayerController.cs
+++ Assets/RyukiArai/PlayerController.cs
@@ -5,14 +5,12 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     Rigidbody2D _rb;
-    GameManager _gm;
     [SerializeField] float _moveSpeed;
 
     // Start is called before the first frame update
     void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
-        _gm = GameObject.Find("GameManager").GetComponent<GameManager>();
     }
 
     // Update is called once per frame
@@ -26,6 +24,7 @@ public class PlayerController : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        _gm.GetItem(collision.gameObject.name);
+        var obj = collision.gameObject.GetComponent<Items>();
+        GameManager.Instance.GetItem(obj.ID,obj.Score);
     }
 }
