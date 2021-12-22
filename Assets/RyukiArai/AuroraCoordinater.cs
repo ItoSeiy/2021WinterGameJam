2a618d7b4a94f91381a003e3fbdadc1e4ba3c11c
Ryu-0824 <83778052+Ryuki-Arai@users.noreply.github.com>
2021-12-22 12:10:59 +0900

    Aurora追加

---
+++ Assets/RyukiArai/AuroraCoordinater.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AuroraCoordinater : MonoBehaviour
+{
+    [SerializeField] Vector2[] Apos;
+    float time;
+    // Start is called before the first frame update
+    void Start()
+    {
+        time = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        if(time >= 3.0f)
+        {
+            this.gameObject.transform.position = Apos[(int)Random.Range(0, Apos.Length)];
+            time = 0;
+        }
+    }
+}
