333e51e811da202bc2994cd7b6e7b461184329a3
sakezuma <sakezuma@gmail.com>
2021-12-22 15:56:08 +0900

    UIManager修正

--- Assets/Niituma/UIManager.cs
+++ Assets/Niituma/UIManager.cs
@@ -8,7 +8,7 @@ public class UIManager : MonoBehaviour
     [SerializeField] TextMeshProUGUI _scoreText = null;
     [SerializeField] TextMeshProUGUI _gameTimer = null;
     // Start is called before the first frame update
-    void Start()
+    void Update()
     {
         _scoreText.text = "Score:"+ GameManager.Instance.Score.ToString("0000");
 
