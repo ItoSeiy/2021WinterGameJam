3aacd38cc7ffbc87072f39408f70099352ad5d15
OnoueHiroaki <hirophon18@ezweb.ne.jp>
2021-12-22 17:03:17 +0900

    オーロラに機能を追加しました。

--- Assets/RyukiArai/AuroraCoordinater.cs
+++ Assets/RyukiArai/AuroraCoordinater.cs
@@ -1,25 +1,60 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class AuroraCoordinater : MonoBehaviour
 {
     [SerializeField] Vector2[] Apos;
     float time;
-    // Start is called before the first frame update
+    bool _timeflag;
+    [SerializeField]
+    float _fadeSpeed;
+    float _alpha;
+    bool _fadeFlag;
+    Image _image;
     void Start()
     {
+        _image = GetComponentInChildren<Image>();
+        _alpha = _image.color.a;
         time = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        time += Time.deltaTime;
-        if(time >= 3.0f)
+        if (!_fadeFlag)
         {
-            this.gameObject.transform.position = Apos[(int)Random.Range(0, Apos.Length)];
-            time = 0;
+            FadeOut();
+            time += Time.deltaTime;
+            Debug.Log(time);
+            if (time > 3 && _alpha <= 0)
+            {
+                _timeflag = true;
+                _fadeFlag = true;
+                time = 0;
+                Debug.Log(_timeflag);
+                this.gameObject.transform.position = Apos[(int)Random.Range(0, Apos.Length)];
+                
+            }
         }
+        else
+        {
+            FadeIn();
+        }
+    }
+    //_fadeFlagがtrueのときはフェードイン
+    void FadeIn()
+    {
+        _alpha += _fadeSpeed * Time.deltaTime;
+        _image.color = new Color(255, 255, 255, _alpha);
+        if (_alpha >= 1)
+        {
+            _fadeFlag = false;
+        }
+    }
+    void FadeOut()
+    {
+        _alpha -= _fadeSpeed * Time.deltaTime;
+        _image.color = new Color(255, 255, 255, _alpha);
     }
 }
